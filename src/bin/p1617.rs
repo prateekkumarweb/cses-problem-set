5541351659b3022034a8c2afe929c0d5c9b6b03c
Prateek Kumar <me@prateek.page>
2025-05-25 00:45:07 +0400

    bit strings

---
+++ src/bin/p1617.rs
@@ -0,0 +1,22 @@
+const MOD: usize = 1_000_000_007;
+
+fn main() {
+    let n = std::io::stdin()
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap()
+        .parse::<usize>()
+        .unwrap();
+    let mut power = 1;
+    let mut pow_2 = 2;
+    for i in 0..32 {
+        let mask = 1 << i;
+        let bit_is_set = (mask & n) != 0;
+        if bit_is_set {
+            power = (power * pow_2) % MOD;
+        }
+        pow_2 = (pow_2 * pow_2) % MOD;
+    }
+    println!("{}", power);
+}
