db226ea65d0b6e92ad775770669936a2c41d3f33
Prateek Kumar <me@prateek.page>
2025-05-25 00:53:58 +0400

    trailing zeros

---
+++ src/bin/p1618.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let n = std::io::stdin()
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap()
+        .parse::<usize>()
+        .unwrap();
+    let mut res = 0;
+    for c in 1.. {
+        let f = 5usize.pow(c);
+        let a = n / f;
+        if a == 0 {
+            break;
+        }
+        res += a;
+    }
+    println!("{}", res);
+}
