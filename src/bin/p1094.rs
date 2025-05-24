f38578cb348e503a0ade71c90cd23bf0f402b762
Prateek Kumar <me@prateek.page>
2025-05-24 16:59:06 +0400

    increasing array

---
+++ src/bin/p1094.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let mut lines = std::io::stdin().lines();
+    lines.next();
+    let line = lines.next().unwrap().unwrap();
+    let mut count = 0;
+    let mut prev = 0;
+    for x in line.split_ascii_whitespace() {
+        let x: usize = x.parse().unwrap();
+        if x < prev {
+            count += prev - x;
+        } else {
+            prev = x;
+        }
+    }
+    println!("{}", count);
+}
