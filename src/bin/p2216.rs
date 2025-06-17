a72931eb79b5adf6ea60ccf6f8e69e035e17a085
Prateek Kumar <me@prateek.page>
2025-06-17 10:24:48 +0400

    collecting numbers

---
+++ src/bin/p2216.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let mut lines = std::io::stdin().lines();
+    let n = lines.next().unwrap().unwrap().parse::<i64>().unwrap();
+    let nums = lines
+        .next()
+        .unwrap()
+        .unwrap()
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
+    let indices_map = nums
+        .iter()
+        .enumerate()
+        .map(|(i, &num)| (num, i + 1))
+        .collect::<std::collections::BTreeMap<_, _>>();
+    let mut result = 1;
+    for i in 1i64..n {
+        if indices_map[&(i + 1)] < indices_map[&i] {
+            result += 1;
+        }
+    }
+    println!("{}", result);
+}
